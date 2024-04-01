9cfab9585af273a25a69fef64c6a58077e1fbfc2
ck888chang <164529890+ck888chang@users.noreply.github.com>
2024-04-01 23:53:46 +0800

    更新 test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import redshift_connector
 
 def mysqlconnect():
     # Connect to MySQL
-    cnx = pymysql.connect(host="jvdckrdsa.ckht8fbw26hy.ap-northeast-1.rds.amazonaws.com", port=3306, user="admin", passwd="7taw996d", db="fatpit_staging")
+    cnx = pymysql.connect(host="jvsa.cby.ap-northeast-1.rds.amazonaws.com", port=3306, user="xxxn", passwd="zxxx", db="xxx")
     cursor = cnx.cursor()
 
     # Query data from MySQL
@@ -19,11 +19,11 @@ def mysqlconnect():
 def Redshiftconnect():
     # Connect to Redshift
     conn = redshift_connector.connect(
-         host='jvdckshift.210199513596.ap-northeast-1.redshift-serverless.amazonaws.com',
+         host='xxxxift.210199513596.ap-northeast-1.redshift-serverless.amazonaws.com',
          database='dev',
          port=5439,
          user='admin',
-         password='7taw996D'
+         password='xxxxx'
       )
     cur = conn.cursor()
 
@@ -51,10 +51,10 @@ if __name__ == '__main__':
 ## JVDCKEC2A 172.30.0.22 (ec2-18-176-54-176.ap-northeast-1.compute.amazonaws.com)
 ## JVDCKEC2B 172.30.0.89 (ec2-18-183-176-33.ap-northeast-1.compute.amazonaws.com)
 ## JVDckrdsa 172.30.0.19 (54.168.65.10)
-#  jvdckrdsa.ckht8fbw26hy.ap-northeast-1.rds.amazonaws.com
-#  jvdckshift.210199513596.ap-northeast-1.redshift-serverless.amazonaws.com
+#  jvdsa.ckht8fbw26hy.ap-northeast-1.rds.amazonaws.com
+#  jvdft.210199513596.ap-northeast-1.redshift-serverless.amazonaws.com
 #  172.30.2.84
 
-##  jdbc:mysql://jvdckrdsa.ckht8fbw26hy.ap-northeast-1.rds.amazonaws.com:3306/fatpit_staging
+##  jdbc:mysql://jvdckrdsa.ckht8fbw26hy.ap-northeast-1.rds.amazonaws.com:3306/fatpg
 
 ## jdbc:redshift://jvdckshift.210199513596.ap-northeast-1.redshift-serverless.amazonaws.com:5439/dev?tcpKeepAlive=true
